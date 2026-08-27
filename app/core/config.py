ac052e7c6e4f1027144d81e0831898ae3ea07918
ElMathidones <seuemail@exemplo.commath.mendez.007@gmail.com>
2026-08-26 23:44:34 -0300

    chore: add database dependencies

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     app_name: str = "Help Desk API"
     app_version: str = "0.1.0"
     debug: bool = False
+    database_url: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
